fd36272a9c7f8e73fc44647c2f92cf23e1944975
ashiwaju <ikumez8@gmail.com>
2022-04-20 15:02:30 +0100

    The game is complete and can be played but needs proper commenting. Will we worked on in the new release.

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing Game")
+screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.go_up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    if player.ycor() > 280:
+        player.reset_position()
+        scoreboard.p_level()
+        car_manager.level_up()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-220, 250)
+        self.write(f"LEVEL :{self.level}", align="center", font=FONT)
+
+    def p_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game over", align="center", font=FONT)
+
